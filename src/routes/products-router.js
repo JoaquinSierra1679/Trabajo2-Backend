153a40141499630407f18e4543da95d2f82b9725
Joaquin Sierra <joaquinsierra1679@gmail.com>
2023-03-22 14:13:58 -0300

    entrego websockets

--- src/routes/products-router.js
+++ src/routes/products-router.js
@@ -8,12 +8,11 @@ productsRouter.get("/", async (req, res)=>{
     try{
         const products = await manager.getProducts()
         const {limit} = req.query
-
+    
         if (limit){
             const productsLimit = products.slice(0,limit)
             return res.send({status: "succes", payload: productsLimit})
         }
-
         res.send({status: "succes", payload: products})
     } catch (err){
         res.status(404).send({status: "error", error: `${err}`})
@@ -35,35 +34,41 @@ productsRouter.post("/", async (req,res)=>{
         // En el body no envío "thumbail" ni "status", los defino por defecto hasta que tenga que cambiarlo
         const {title, description, price, thumbail=[], code, stock, status=true, category} = req.body
         await manager.addProduct(title, description, parseInt(price), thumbail, code, parseInt(stock), status, category)
+            req.io.emit("new-product", req.body)
+            res.send({status: "succes", payload: req.body})
+        }catch(err){
+            res.status(404).send({status: "error", error: `${err}`})
+        }
+    })
+    
+    productsRouter.put("/:pid", async (req, res)=>{
+        try{
+            const {pid} = req.params
+            const id = parseInt(pid)
+            await manager.updateProduct(id, req.body)
+    
+            const products = await manager.getProducts()
+            req.io.emit("update-product", products)
+    
+            res.send({status: "succes", payload: await manager.getProductById(id)})
+        }catch(err){
+            res.status(404).send({status: "error", error: `${err}`})
+        }
+    })
 
-        res.send({status: "succes", payload: req.body})
-    }catch(err){
-        res.status(404).send({status: "error", error: `${err}`})
-    }
-})
+    productsRouter.delete("/:pid", async(req, res)=>{
+        try{
+            const {pid} = req.params
+            const id = parseInt(pid)
+            await manager.deleteProduct(id)
+    
+            const products = await manager.getProducts()
+            req.io.emit("delete-product", products)
+    
+            res.send({status: "succes", payload: "Producto eliminado"})
+        } catch(err){
+            res.status(404).send({status: "error", error: `${err}`})
+        }
+    })
 
-productsRouter.put("/:pid", async (req, res)=>{
-    try{
-        const {pid} = req.params
-        const id = parseInt(pid)
-        await manager.updateProduct(id, req.body)
-
-        res.send({status: "succes", payload: await manager.getProductById(id)})
-    }catch(err){
-        res.status(404).send({status: "error", error: `${err}`})
-    }
-})
-
-productsRouter.delete("/:pid", async(req, res)=>{
-    try{
-        const {pid} = req.params
-        const id = parseInt(pid)
-        await manager.deleteProduct(id)
-
-        res.send({status: "succes", payload: "Producto eliminado"})
-    } catch(err){
-        res.status(404).send({status: "error", error: `${err}`})
-    }
-})
-
-export default productsRouter
+    export default productsRouter
